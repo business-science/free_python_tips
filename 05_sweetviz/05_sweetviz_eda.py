985056a4d614262c294fdacc1755e8a6665ccadc
Matt Dancho <mdancho@gmail.com>
2021-08-02 20:53:16 -0400

    sweetviz

---
+++ 05_sweetviz/05_sweetviz_eda.py
@@ -0,0 +1,58 @@
+# PYTHON TIPS ----
+# TIP 005 | Sweetviz ----
+#
+# 👉 For Weekly Python-Tips, Sign Up Here:
+#    https://mailchi.mp/business-science/python_tips_newsletter
+
+# LIBRARIES ----
+
+# GitHub: https://github.com/fbdesignpro/sweetviz
+
+# pip install sweetviz
+
+import pandas as pd
+import sweetviz as sv
+
+import plotnine as pn
+
+# DATASET ----
+
+mpg_df = pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/mpg.csv")
+
+mpg_df
+
+# SWEETVIZ ----
+
+report = sv.analyze(
+    mpg_df,
+    target_feat='mpg'
+)
+
+report.show_html(
+    filepath = "05_sweetviz/report.html"
+)
+
+# PLOTNINE -----
+# - Learn plotnine in DS4B 101-P
+
+pn.ggplot(
+    pn.aes(
+        'horsepower', 'acceleration', 
+        color = 'mpg',
+        size  = 'mpg'
+    ),
+    data = mpg_df
+) + \
+    pn.geom_point(alpha = 0.5) + \
+    pn.geom_smooth(
+        method = "lowess", 
+        span   = 0.3,
+        color  = 'blue'
+    ) + \
+    pn.guides(size = False) + \
+    pn.theme_classic()
+    
+
+# LEARNING MORE:
+# Python for Data Science Automation Course: 
+# https://university.business-science.io/p/python-for-data-science-automation-ds4b-101p
